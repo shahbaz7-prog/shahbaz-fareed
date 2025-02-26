24cd1a339de2fa18419eb99b04b8439d7a68adb7
shahbaz fareed <shahbazpola71@gmail.com>
2025-02-26 23:18:44 +0500

    assignment 1
    fa23-bba-120

---
+++ assingment 1.py
@@ -0,0 +1,32 @@
+Python 3.12.9 (tags/v3.12.9:fdb8142, Feb  4 2025, 15:27:58) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # Display several numbers
+>>> print(8, 67, 35, 83, 76, 67)
+8 67 35 83 76 67
+>>> # Solve and show the summation of 64 + 32
+>>> print("64 + 32 =", 64 + 32)
+64 + 32 = 96
+>>> # Solve and show summation using variables
+>>> x = 64
+>>> KeyboardInterrupt
+>>> x=64
+>>> y=32
+>>> sum=x+y
+>>> print("sum =", sum)
+sum = 96
+\
+>>> # Print the word 'Lucky' inside a variable
+>>> s = "Lucky"
+>>> print(s)
+Lucky
+>>> # Print today's date in the format "Today is 2/2/2016"
+>>> day = 2
+>>> month = 26
+>>> year = 2025
+>>> print("Today is", day, "/", month, "/", year)
+Today is 2 / 26 / 2025
+>>> # Ask user for phone number and display it
+>>> phone_number = input("03088092027: ")
+03088092027: 
+>>> print ("Your phone number is: ", phone_number)
+Your phone number is:  
